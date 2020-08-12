b19edf687fd1c1ce6feb36a5ce0e764a1073483d
omniaatefmohamed <68192568+omniaatefmohamed@users.noreply.github.com>
2020-08-12 13:56:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-var projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
